8563febeba3f819001e4e56d401f4402dc5a60eb
Bruno <bruno-8-2010@hotmail.com>
2022-07-19 16:36:38 -0300

    add toc

--- scripts/script_elt.py
+++ scripts/script_elt.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import pandas as pd
 url = 'https://raw.githubusercontent.com/bmorbin/ML_Project/main/data_raw.csv'
 df = pd.read_csv(url)
 print(df.head(5))
-=======
-import pandas as pd
-url = 'https://raw.githubusercontent.com/bmorbin/ML_Project/main/data_raw.csv'
-df = pd.read_csv(url)
-print(df.head(5))
->>>>>>> 3ccef94d7154cd69cd0fd4fc6048801a19467be8
